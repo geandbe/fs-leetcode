9a7d8499e209cfbc460b42d721bdb550429b7f35
Gene Belitski <gene.belitski@gmail.com>
2019-03-11 14:37:26 -0400

    uniqueHead redone, empty arg

--- Problems/Problem0003.fsx
+++ Problems/Problem0003.fsx
@@ -1,12 +1,19 @@
 ﻿// Leetcode Problem 3 Longest Substring Without Repeating Characters
 
-let uniqueSubstr s =
+let uniqueMaxSubstr (s: string) =
     let rec uniqueHead (s:string) (ls: string list) = 
         let l = s |> Seq.mapi(fun i c -> s.IndexOf(c) = i) |> Seq.takeWhile(id) |> Seq.length
-        if l > 1 then uniqueHead s.[l..] (s.[..l-1]::ls) elif l = 1 then s.[..l-1]::ls else ls
-    uniqueHead s [] |> List.maxBy (fun s -> s.Length) |> fun s -> (s, s.Length)
+        if l = s.Length && l = 0 then ls
+        elif l = s.Length then s::ls
+        else uniqueHead s.[s.IndexOf(s.[(l)]) + 1 .. ] (s.[.. (l - 1)]::ls)
+    match s with
+    | "" -> ("",0)
+    | _ -> uniqueHead s [] |> List.maxBy (fun s -> s.Length) |> fun s -> (s, s.Length)
 
-uniqueSubstr "abcabc"   // ("abc", 3)
-uniqueSubstr "bbbbb"    // ("b", 1)
-uniqueSubstr "pwwkew"   // ("wke", 3)
-uniqueSubstr "abcdef"   // ("abcdef", 6)
+uniqueMaxSubstr "pwwkew"
+uniqueMaxSubstr "bbbbb"
+uniqueMaxSubstr " "
+uniqueMaxSubstr "dvdf"
+uniqueMaxSubstr "tmmzuxt"
+uniqueMaxSubstr "dvd"
+uniqueMaxSubstr ""
