3efe1e19195e72f7e2b21797c49926dc59dd6e28
geandbe <gene.belitski@gmail.com>
2019-03-10 22:35:18 -0400

    Problem 3 Case when last string l > 1 is unique

--- Problems/Problem0003.fsx
+++ Problems/Problem0003.fsx
@@ -3,10 +3,10 @@
 let uniqueSubstr s =
     let rec uniqueHead (s:string) (ls: string list) = 
         let l = s |> Seq.mapi(fun i c -> s.IndexOf(c) = i) |> Seq.takeWhile(id) |> Seq.length
-        if l > 1 then uniqueHead s.[l..] (s.[..l-1]::ls) else s.[..l-1]::ls
+        if l > 1 then uniqueHead s.[l..] (s.[..l-1]::ls) elif l = 1 then s.[..l-1]::ls else ls
     uniqueHead s [] |> List.maxBy (fun s -> s.Length) |> fun s -> (s, s.Length)
 
-uniqueSubstr "abcabcbb"
-uniqueSubstr "bbbbb"
-
-uniqueSubstr "pwwkew"
+uniqueSubstr "abcabc"   // ("abc", 3)
+uniqueSubstr "bbbbb"    // ("b", 1)
+uniqueSubstr "pwwkew"   // ("wke", 3)
+uniqueSubstr "abcdef"   // ("abcdef", 6)
