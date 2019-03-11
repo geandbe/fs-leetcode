814f64c52688c9e27eb45f7ec3bf2a0dcfbf8c64
geandbe <gene.belitski@gmail.com>
2019-03-10 22:17:47 -0400

    Problem 3

---
+++ Problems/Problem0003.fsx
@@ -0,0 +1,18 @@
+﻿
+
+open System.Linq.Expressions
+open System.Linq.Expressions
+
+let hs = HashSet()
+// Leetcode Problem 3 Longest Substring Without Repeating Characters
+
+let uniqueSubstr s =
+    let rec uniqueHead (s:string) (ls: string list) = 
+        let l = s |> Seq.mapi(fun i c -> s.IndexOf(c) = i) |> Seq.takeWhile(id) |> Seq.length
+        if l > 1 then uniqueHead s.[l..] (s.[..l-1]::ls) else s.[..l-1]::ls
+    uniqueHead s [] |> List.maxBy (fun s -> s.Length) |> fun s -> (s, s.Length)
+
+uniqueSubstr "abcabcbb"
+uniqueSubstr "bbbbb"
+
+uniqueSubstr "pwwkew"
