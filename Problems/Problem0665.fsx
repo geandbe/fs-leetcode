6e4b9e17e15156a8fd100cc094ce5d061302fd4c
Gene Belitski <gene.belitski@gmail.com>
2019-03-17 11:47:30 -0400

    Problem 0665 added in full

--- Problems/Problem0665.fsx
+++ Problems/Problem0665.fsx
@@ -1,1 +1,15 @@
-﻿
+﻿let isNondec ls =
+    let rec checkOrder ls isFlipped =
+        match ls with
+        | [x] -> true
+        | [] -> true
+        | x::y::t -> if x <= y then checkOrder (y::t) isFlipped elif isFlipped then false else checkOrder (y::t) true
+    checkOrder ls false
+
+
+isNondec [1;2;3]
+isNondec [2;1]
+isNondec [1;2]
+isNondec [4;2;3]
+isNondec [4;2;1]
+isNondec [-1;4;2;3]
