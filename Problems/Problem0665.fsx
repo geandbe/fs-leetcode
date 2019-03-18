c7693da7a7eee6acc126e246191e3852d6f433de
Gene Belitski <gene.belitski@gmail.com>
2019-03-18 16:19:57 -0400

    Problem 665 further tuning

--- Problems/Problem0665.fsx
+++ Problems/Problem0665.fsx
@@ -1,10 +1,14 @@
 ﻿let isNondec ls =
-    let rec checkOrder ls isFlipped =
+    let rec checkOrder ls isFlipped p =
         match ls with
         | [x] -> true
         | [] -> true
-        | x::y::t -> if x <= y then checkOrder (y::t) isFlipped elif isFlipped then false else checkOrder (y::t) true
-    checkOrder ls false
+        | x::y::t when isFlipped -> if x > y then false
+                                    else checkOrder (y::t) true x
+        | x::y::t -> if x <= y then checkOrder (y::t) false x
+                     else checkOrder ((max p y)::y::t) true p
+                     
+    checkOrder ls false System.Int32.MinValue
 
 
 isNondec [1;2;3]
@@ -13,3 +17,6 @@ isNondec [1;2]
 isNondec [4;2;3]
 isNondec [4;2;1]
 isNondec [-1;4;2;3]
+isNondec [3;4;2;3]
+isNondec [3;3;2;2]
+
